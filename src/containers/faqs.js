57200e7a09c9a7da5431fd3b07dd127f0c8604a5
mofath <mohamed.elgaedi@gmail.com>
2020-10-14 06:40:22 +0200

    Added OptForm component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Accordion from '../components/accordion';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs.json';
 
 
@@ -8,14 +8,23 @@ export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {
-                faqsData.map((item) =>
-                    <Accordion.Item key={item.id}>
-                        <Accordion.Header>{item.header}</Accordion.Header>
-                        <Accordion.Body>{item.body}</Accordion.Body>
-                    </Accordion.Item>
-                )
-            }
+            <Accordion.Item>
+                {
+                    faqsData.map((item) =>
+                        <Accordion.Item key={item.id}>
+                            <Accordion.Header>{item.header}</Accordion.Header>
+                            <Accordion.Body>{item.body}</Accordion.Body>
+                        </Accordion.Item>
+                    )
+                }
+            </Accordion.Item>
+            <OptForm>
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+            </OptForm>
+
         </Accordion>
     )
 }
